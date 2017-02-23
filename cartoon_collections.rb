0c0e48429c38ac022b74b67fa13df36461a3596c
Runnan Zhong <r.tina.zhong@gmail.com>
2017-02-23 16:46:00 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
+  dwarves.to_enum.with_index(1).each { |dwarf, i| puts "#{i}. #{dwarf}" }
+end
+
+def summon_captain_planet(array)
+  array.map {|element| element.capitalize << "!"}
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  if array.length >= 4
+    true
+  else
+    false
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(maybe_with_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  maybe_with_cheese.find {|cheese| cheese_types.include?(cheese)}
+
 end
